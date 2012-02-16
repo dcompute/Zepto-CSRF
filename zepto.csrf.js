bb632522f191f176df6f7f68ca81319a29b0d23d
Daniel Lacy <dan@daniellacy.com>
2012-02-16 14:11:40 -0500

    Rewriting plugin to use extend method. Also wrapping plugin within it's own scope so as not to interfere with stuffs.

--- zepto.csrf.js
+++ zepto.csrf.js
@@ -1,41 +1,83 @@
-$.ajaxSettings.beforeSend = function (xhr, settings) {
-    function getCookie (name) {
-        var cookieValue,
-            cookies,
-            cookie,
-            i;
+/*
+ * Zepto.csrf.js is freely distributable under the MIT license.
+ *
+ * @author Daniel Lacy <dan@daniellacy.com>
+ * @version 1.1
+ * @since 2011-10-19
+ *
+ * Copyright (c) 2011 Daniel Lacy (daniellacy.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+**/
+(function ($) {
+    /*
+     * Retrieves the value of a cookie by the given key.
+     *
+     * @param key, (string) Name of the cookie to retrieve.
+     * @return Value of the given key or null.
+    **/
+    function getCookie (key) {
+        var value = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)');
+        
+        return (value.exec(document.cookie)) ? value[1] : null;
 
-        if (document.cookie && document.cookie != '') {
-            cookies = document.cookie.split(';');
-
-            for (i = 0; i < cookies.length; i++) {
-                cookie = cookies[i].trim();
-
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-
-                    break;
-                }
-            }
-        }
-
-        return cookieValue;
     }
 
+    /*
+     * Checks whether or not a request is originating from ___
+     *
+     * @param url, (string) URL of...
+     * @return
+    **/
     function sameOrigin (url) {
         // url could be relative or scheme relative or absolute
         var sr_origin = '//' + document.location.host,
             origin = document.location.protocol + sr_origin;
 
         // Allow absolute or scheme relative URLs to same origin
-        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+        return (
+                url == origin ||
+                url.slice(0, origin.length + 1
+            ) == origin + '/') ||
+            (
+                url == sr_origin ||
+                url.slice(0, sr_origin.length + 1
+            ) == sr_origin + '/') ||    
             // or any other URL that isn't scheme relative or absolute i.e relative.
             !(/^(\/\/|http:|https:).*/.test(url));
+
     }
 
-    if ( !(/^(GET|HEAD|OPTIONS|TRACE)$/.test(settings.type)) && sameOrigin(settings.url) ) {
-        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-    }
-};
+    /*
+     * Extend Zepto's ajaxSetting.beforeSend method by setting an X-CSRFToken
+     * header on each request.
+    **/
+    $.extend($.ajaxSettings, {
+        beforeSend : function (xhr, settings) {
+            if (
+                !(/^(GET|HEAD|OPTIONS|TRACE)$/.test(settings.type)) &&
+                sameOrigin(settings.url)
+            ) {
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+
+        }
+    });
+
+})(Zepto);
